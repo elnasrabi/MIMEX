2cd291e9026dc060ee29222a11b911d1869c0690
Nishith Sheth <>
2020-05-25 10:04:26 +0530

    Removed History.

--- .history/app/models/consignment-store/consignment-store.test_20200521174738.ts
+++
@@ -1,7 +0,0 @@
-import { HomeStoreModel, HomeStore } from "./home-store"
-
-test("can be created", () => {
-  const instance: HomeStore = HomeStoreModel.create({})
-
-  expect(instance).toBeTruthy()
-})
--- .history/app/models/consignment-store/consignment-store.test_20200521175329.ts
+++
@@ -1,6 +0,0 @@
-import { ConsignmentStoreModel, ConsignmentStore } from "./consignment-store"
-
-test("can be created", () => {
-  const instance: ConsignmentStore = ConsignmentStoreModel.create({})
-  expect(instance).toBeTruthy()
-})
