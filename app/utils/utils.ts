57971460f3cfc26d20f32bec0a32a5aabab3a108
Vijay Singh Chouhan <vijay.singh@tatvasoft.com>
2020-06-16 16:00:12 +0530

    did changes

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -17,7 +17,7 @@ export function translateText(text): string {
   const i18nText = text && translate(text);
   return i18nText;
 }
-export function showAlert(title, desc = "", buttonName = "common.setting", onPress = () => {}) {
+export function showAlert(title, desc = "", buttonName = "common.ok", onPress = () => {}) {
   Alert.alert(
     translateText(title),
     translateText(desc),
